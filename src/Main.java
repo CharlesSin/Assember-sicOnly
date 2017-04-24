e823792a30ab9095cd8e9fee1ff918b317bcedfb
Simpson <charlie40503@gmail.com>
2017-04-24 21:16:32 +0800

    新增輸出txt檔功能

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
 	public static void main(String[] args) {
 		Source program = new Source();
 		Card card = new Card(program);
+		Output output = new Output(program, card);
 		for(int i = 0; i < program.getCount(); i++){
-			//System.out.printf("%04X\t%06X\t%s\t%s\t%s\t%s\t%b\n", program.getLocation(i), program.getObject(i), program.getLabel(i), program.getOperation(i), program.getOperand(i), program.getComment(i), program.getX(i));
 			if(program.getObject(i) == "FFFFFF")
 				System.out.printf("%04X %-8s %-6s %-18s      \t%-31s\n", program.getLocation(i), program.getLabel(i), program.getOperation(i), program.getOperand(i), program.getComment(i));
 			else if(program.getX(i))
---
+++ src/Output.java
@@ -0,0 +1,59 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Output {
+	private String line = null;
+	Output(Source p, Card c){
+		buildTable(p);
+		buildCard(c);
+	}
+	
+	private void buildTable(Source program){
+		BufferedWriter output = null;
+		try {
+            output = new BufferedWriter(new FileWriter(new File("table.txt")));
+            for(int i = 0; i < program.getCount(); i++){
+    			if(program.getObject(i) == "FFFFFF")
+    				line = String.format("%04X %-8s %-6s %-18s      \t%-31s\r\n", program.getLocation(i), program.getLabel(i), program.getOperation(i), program.getOperand(i), program.getComment(i));
+    			else if(program.getX(i))
+    				line = String.format("%04X %-8s %-6s %-18s %-6s\t%-31s\r\n", program.getLocation(i), program.getLabel(i), program.getOperation(i), program.getOperand(i) + ",X", program.getObject(i), program.getComment(i));
+    			else
+    				line = String.format("%04X %-8s %-6s %-18s %-6s\t%-31s\r\n", program.getLocation(i), program.getLabel(i), program.getOperation(i), program.getOperand(i), program.getObject(i), program.getComment(i));
+    			output.write(line);
+            }
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        } finally {
+          if ( output != null ) {
+            try {
+				output.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+          }
+        }
+	}
+	
+	private void buildCard(Card card){
+		BufferedWriter output = null;
+		try {
+            output = new BufferedWriter(new FileWriter(new File("card.txt")));
+            output.write(String.format("H " + card.getH() + "\r\n"));
+    		for(int i = 0; i < card.SizeOfT(); i++)
+    			output.write(String.format("T " + card.getT(i) + "\r\n"));
+    		output.write(String.format("E " + card.getE() + "\r\n"));
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        } finally {
+          if ( output != null ) {
+            try {
+				output.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+          }
+        }
+	}
+}
